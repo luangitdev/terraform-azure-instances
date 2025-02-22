87855a41635b51439d630bbdf881805287fc6548
luangitdev <luaninveste@gmail.com>
2025-02-21 23:28:16 -0300

    ops: adicionar saída com o ip público

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "vm_id" {
   value       = azurerm_virtual_machine.vm.id
 }
 
+output "public_ip" {
+  description = "IP público da máquina virtual"
+  value       = azurerm_public_ip.public_ip.ip_address
+}
+
 output "private_ip" {
   description = "IP privado da máquina virtual"
   value       = azurerm_network_interface.nic.private_ip_address
